b5c71e9e178371ad5fbef3271f3f7a0c1853d02e
anmvk58 <an.cnttk58@gmail.com>
2024-09-15 22:37:57 +0700

    init project and add application.properties

---
+++ src/test/java/com/anmv/kafka/KafkaMsStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anmv.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaMsStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
